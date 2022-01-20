ac7193096b6aeb69a5f8f88e718a867394ec2d76
Maria22 <m.ushakova22@gmail.com>
2022-01-20 14:14:03 +0100

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const stack = [];
+  const object = bracketsConfig.reduce((acc,item) => {
+    acc[item[1]] = item[0]
+    return acc;
+  },{});
+  for (let i=0; i<str.length; i++) {
+    let currentElement = str[i];
+    let topElement = stack[stack.length-1];
+    if(Object.values(object).includes(currentElement) && (topElement === currentElement) && (object[currentElement] === currentElement)){
+      stack.pop(currentElement);
+    }
+    else if(Object.values(object).includes(currentElement)){
+      stack.push(currentElement);
+    }
+    else{
+      if(stack.length === 0) {
+        return false;
+      }
+    
+    if(object[currentElement] === topElement){
+      
+      stack.pop();
+    }
+    else {
+      return false;
+    }
+    }  
+  }
+ return stack.length === 0;
 }
